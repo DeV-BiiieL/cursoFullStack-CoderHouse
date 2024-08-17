46da4bb7f1af68dc2479414c10c135193acbeecd
Dev-BiiieL <dev-biiiel@outlook.com>
2024-08-17 15:56:30 -0300

    atualizando aulas do curso

---
+++ JavaScript/Aula-02/app.js
@@ -0,0 +1,28 @@
+/*const nome = 'Gabriel'
+
+const sobrenome = 'Zenaro'
+
+let anoAtual = 2024
+
+let idadeHoje = 39
+
+const fullName = nome + ' ' + sobrenome
+
+const fullName2 = `${nome}` + `${sobrenome}`
+
+let anoNascimento = anoAtual - idadeHoje
+
+console.log(fullName2)*/
+
+// alert('Hello World')
+
+// const nome = prompt('Insira o seu Nome:')
+
+// console.log(nome)
+
+// alert('Seu nome é:' + ' ' + nome)
+
+let numero1 = Number(prompt('Digite um número'))
+let numero2 = Number(prompt('Digite outro Número:'))
+
+console.log(numero1 + numero2)
